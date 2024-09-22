312605bde8754af634f737716f2616b2ca3091b2
Vivian Myers <vmyers@smith.edu>
2024-09-22 15:05:13 -0400

    Conversation Loop

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,98 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
+
 class Conversation {
 
+  /**
+   * Scans users input to determine if a word change needs to occur
+   * @param str
+   * @return T/F depending on whether there are any key words
+   */
+  public static boolean scan(String str){
+    return str.contains("I") || str.contains("am") || str.contains("me")|| str.contains("you") || str.contains("my") || str.contains("your");
+  }
+  /**
+   * This method replaces any key words with its counterpart, making sure each key word is not part of a bigger word (e.g. I in Idaho)
+   * @param str
+   * @return String containing all replaced words w/ a question mark if any words were switched
+   */
+  public static String changeWords(String str){
+    String returnStr = str;
+
+    returnStr = returnStr.replaceAll("\\bI\\b", "You");
+    returnStr = returnStr.replaceAll("\\bam\\b", "are");
+    returnStr = returnStr.replaceAll("\\bme\\b", "you");
+    returnStr = returnStr.replaceAll("\\byou\\b", "I");
+    returnStr = returnStr.replaceAll("\\bmy\\b", "your");
+    returnStr = returnStr.replaceAll("\\byour\\b", "my");
+
+    if(returnStr != str){
+      return returnStr + "?";
+    }
+    return str;
+  }
+
+  /**
+   * main
+   * @param arguments
+   */
   public static void main(String[] arguments) {
     // You will start the conversation here.
+
+  int rounds = 0;
+  Random rand = new Random();
+  ArrayList<String> transcript = new ArrayList<>();
+  String[] responses = {"Hm...", "Cool!", "Very interesting!", "Really?"};
+
+  System.out.println("How many rounds?");
+  Scanner input = new Scanner(System.in);
+  
+
+  rounds = input.nextInt();
+  System.out.println("Number of Rounds: " + rounds);
+
+
+  System.out.println("Hello! Whats up?");
+  transcript.add("Hello! Whats up?");
+
+  //conversation loop
+  for(int i = 0; i < rounds; i++){
+    Scanner input2 = new Scanner(System.in);
+    String userInput = input2.nextLine();
+    transcript.add(userInput);
+
+    if(scan(userInput)){
+      if(userInput != changeWords(userInput)){
+        System.out.println(changeWords(userInput));
+        transcript.add(changeWords(userInput));
+      }else{
+        transcript.add(responses[rand.nextInt(responses.length)]);
+        System.out.println(transcript.getLast());
+       
+      }
+
+    }else{
+      transcript.add(responses[rand.nextInt(responses.length)]);
+      System.out.println(transcript.getLast());
+      
+    }
+    
+
+
+  }// end conversation loop
+
+  System.out.println("OK, Bye!");
+  transcript.add("OK, Bye!");
+  System.out.println("\nTRANSCRIPT:");
+  
+
+  for(int i = 0; i<transcript.size(); i++){
+    System.out.println(transcript.get(i));
+  }
+
+
+
   }
 }
--- reflection.md
+++ reflection.md
@@ -1,7 +1,27 @@
 Use this file to record your reflection on this assignment.
 
 - Which classmates did you consult with while working on this assignment?
+
+Sophia Kwak, Ruby Hong
+
 - Which session(s) of TA / office hours did you attend?
+
+Sunday 1-3pm. They helped me test weird cases (like if round s= 0, more than one mirror word)
+
 - What are your initial impressions of Java? 
+
+It is a lot more tedious than python, but this can help me make less mistakes.
+u
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+
+Flexibility vs structure (java focuses on structure while python focuses on flexibility. Both have their pros and cons but show the versatility of programming)
+
+Speed vs simplicity (java is faster but python is simpler. Again, both have their pros and cons)
+
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+Worked: creating a general frame work (conversation loop) and adding detail from there, helper methods
+
+Didn't work: Trying to accomplish the entire project the first time through.
+
+Advice: Make helper methods to keep your code simple and readable or else you will probably get lost and confused
